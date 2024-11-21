79eeacba23237c5ea333b4cb02aad426a471ac5d
Clarisse <yeoginct@gmail.com>
2024-11-21 12:33:57 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -8,15 +8,16 @@ import math
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.stats
+import utils
 
 def getPrior(train) : 
     data_true = train[train["target"]==1]
     est = len(data_true)/len(train)
 
-    moyenne = np.mean(np.array(train['target']))  #calcul de la moyenne 
-    variance = np.std(np.array(train['target']))#calcul de la variance 
+    moyenne = np.mean(np.array(train['target']))  # calcul de la moyenne 
+    variance = np.std(np.array(train['target'])) # calcul de la variance 
   
-    #Calcul de l'intervalle de confiance à 95%
+    # Calcul de l'intervalle de confiance à 95%
     s = scipy.stats.sem(np.array(train['target']))
     i = s * scipy.stats.t.ppf((1 + 0.95) / 2., len(train)-1)
     marge_sup = moyenne+i
@@ -28,15 +29,36 @@ def getPrior(train) :
     }
     return res 
 
-class APrioriClassifier(AbstractClassifier) :
-
-    def __init__(self):
-        pass
+class APrioriClassifier(utils.AbstractClassifier) :
+    def __init__(self, df):
+        self.df=df
 
     def estimClass(self, attrs):
-        
-
+        if (not attrs):
+            return 0
+        elif (attrs["cp"]==0 | attrs["exang"]==1 | attrs["oldpeak"]>5 | attrs["ca"]>0):
+            return 0
+        else : 
+            return 1
     
-
-
     def statsOnDF(self, df):
+
+        vp=0
+        vn=0
+        fp=0
+        fn=0
+        nb_positifs = len(df[df['target']==1])
+        nb_negatifs = len(df[df['target']==0])
+        for i in range(len(df)):
+            dic = utils.getNthDict(df,i)
+            if (self.estimClass(dic)==1 & dic['target']==1):
+                vp+=1
+            elif (self.estimClass(dic)==1 & dic['target']==0):
+                fp+=1
+            elif (self.estimClass(dic)==0 & dic['target']==1):
+                fn+=1
+            elif (self.estimClass(dic)==0 & dic["target"]==0):
+                vn+=1
+        dic_res = {'VP' : vp, 'VN' : vn, 'FP' : fp, 'FN' : fn, 'Précision' : vp/(vp+fp)}
+        #'Rappel' : vp/(vp+fn)
+        return dic_res
